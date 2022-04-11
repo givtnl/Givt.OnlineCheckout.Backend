fd1066671ef378533b78ffa1849524bbd2f4f01f
Alex <alex@givtapp.net>
2022-04-11 13:11:44 +0200

    Added warning comment tot PaymentMethod

--- src/Integrations/Givt.OnlineCheckout.Integrations.Interfaces/Models/PaymentMethod.cs
+++ src/Integrations/Givt.OnlineCheckout.Integrations.Interfaces/Models/PaymentMethod.cs
@@ -2,12 +2,12 @@
 
 public enum PaymentMethod
 {
-    Bancontact = 0,
-    Card = 1,
-    Ideal = 2,
-    Sofort = 3,
-    Giropay = 4,
-    EPS = 5,
+    Bancontact = 0, // do not change unless the mapping in Stripe to stripe names is changed too
+    Card = 1,       // idem
+    Ideal = 2,      // idem
+    Sofort = 3,     // idem
+    Giropay = 4,    // idem
+    EPS = 5,        // idem
     ApplePay = 6,
     GooglePay = 7,
 }
