7d87efc464b7975a9c9234a96810255efe5ee049
Alex <alex@givtapp.net>
2022-04-11 13:18:02 +0200

    Fixed mapping on Medium Query

--- src/Core/Givt.OnlineCheckout.API/Models/Mediums/GetMediumResponse.cs
+++ src/Core/Givt.OnlineCheckout.API/Models/Mediums/GetMediumResponse.cs
@@ -6,8 +6,11 @@ public class GetMediumResponse
 {
     public string Medium { get; set; }
     public string OrganisationName { get; set; }
-    public decimal[] Amounts { get; set; }
-    public PaymentMethod[] PaymentMethods { get; set; }
-    public string ThankYou { get; set; }
+    public string OrganisationLogoLink { get; set; }
     public string Goal { get; set; }
+    public PaymentMethod[] PaymentMethods { get; set; }
+    public string Currency { get; set; }
+    public decimal[] Amounts { get; set; }
+    public string ThankYou { get; set; }
+
 }
