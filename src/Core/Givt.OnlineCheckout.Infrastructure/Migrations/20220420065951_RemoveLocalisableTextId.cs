50a5bdab5e323b65ea2f5fe413a9a969b5556d4d
Alex <alex@givtapp.net>
2022-04-20 09:00:33 +0200

    Removed ID from LocalisableTexts

---
+++ src/Core/Givt.OnlineCheckout.Infrastructure/Migrations/20220420065951_RemoveLocalisableTextId.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Givt.OnlineCheckout.Infrastructure.Migrations
+{
+    public partial class RemoveLocalisableTextId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "OrganisationTexts");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "MediumTexts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Id",
+                table: "OrganisationTexts",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Id",
+                table: "MediumTexts",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
--- src/Core/Givt.OnlineCheckout.Persistance/Entities/LocalisableTexts.cs
+++ src/Core/Givt.OnlineCheckout.Persistance/Entities/LocalisableTexts.cs
@@ -2,7 +2,6 @@
 
 public class LocalisableTexts
 {
-    public long Id { get; set; }
     public string LanguageId { get; set; } // length at least 14 (ca-ES-valencia = catalan spain valencia dialect)
     public string Goal { get; set; }
     public string ThankYou { get; set; }
