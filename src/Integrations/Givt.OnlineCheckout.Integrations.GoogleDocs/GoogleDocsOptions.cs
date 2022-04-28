bce57cbdd8a821667cce09f4dd7b98501ebf5671
Bjorn Derudder <bjorn.derudder@gmail.com>
2022-04-28 16:05:08 +0200

    added the german translations and the brittish correct document

--- src/Integrations/Givt.OnlineCheckout.Integrations.GoogleDocs/GoogleDocsOptions.cs
+++ src/Integrations/Givt.OnlineCheckout.Integrations.GoogleDocs/GoogleDocsOptions.cs
@@ -5,6 +5,8 @@ public class GoogleDocsOptions
     public static string SectionName = "GoogleDocs";
     public string DonationConfirmationNL { get; set; }
     public string DonationConfirmationEN { get; set; }
+    public string DonationConfirmationGB { get; set; }
+    public string DonationConfirmationDE { get; set; }
     public string PrivateKey { get; set; }
     public string ApplicationName { get; set; }
     public string ServiceAccountEmail { get; set; }
