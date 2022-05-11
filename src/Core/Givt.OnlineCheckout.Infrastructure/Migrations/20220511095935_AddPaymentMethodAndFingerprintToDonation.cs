d010261e3c22a9627b1ab50d7eef656eca7c012a
Mike Pattyn <mike@givtapp.net>
2022-05-11 12:04:57 +0200

    Add migration that adds PaymentMethod and fingerprint to DonationData

---
+++ src/Core/Givt.OnlineCheckout.Infrastructure/Migrations/20220511095935_AddPaymentMethodAndFingerprintToDonation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Givt.OnlineCheckout.Infrastructure.Migrations
+{
+    public partial class AddPaymentMethodAndFingerprintToDonation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Fingerprint",
+                table: "Donations",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "PaymentMethod",
+                table: "Donations",
+                type: "smallint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fingerprint",
+                table: "Donations");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Donations");
+        }
+    }
+}
