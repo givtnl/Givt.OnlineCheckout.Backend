daf96d78db494a91790bd876fd7c026fa8f4c639
Alex <alex@givtapp.net>
2022-05-19 14:46:47 +0200

    Add helth check ping (Azure)

---
+++ src/Core/Givt.OnlineCheckout.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Givt.OnlineCheckout.API.Controllers
+{
+    [Route("/api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        // Health check
+        public IActionResult Check()
+        {            
+            return Ok();
+        }
+    }
+}
